2ebd74c41fc7749169bedee7418c93c0c514bf21
jandro1111 <aalatsis@itba.edu.ar>
2021-05-25 21:50:02 -0300

    borre los couts

--- tp final/tp final.cpp
+++ tp final/tp final.cpp
@@ -10,41 +10,6 @@ using namespace std;
 
 int main()
 {
-
-	int num, numoutofrange;
-	num = 0;
-	numoutofrange = 10;
-	string aux;
-	nlohmann::json bloque, tx, vin, vout;
-	std::string file = "ejemplo.json";
-	blockchain prueba(file);
-	bloque = prueba.getblock(num);//pasar num como si fuese un indice
-	tx = prueba.gettx(bloque, 0);
-	tx = prueba.gettx(bloque, num);//pasar num como indice
-	vin = prueba.getvin(tx, num);//pasar num como indice
-	vout = prueba.getvout(tx, num);//pasar num como indice
-
-	cout << "cantidad de bloques: " << prueba.getcantblock() << endl;
-	cout << "id del bloque: " << prueba.getblockid(bloque) << endl;
-	cout << "id del bloque anterior: " << prueba.getpreviousblockid(bloque) << endl;
-	cout << "height: " << prueba.getheight(bloque) << endl;
-	cout << "merkleroot: " << prueba.getmerkleroot(bloque) << endl;
-	cout << "nonce: " << prueba.getnonce(bloque) << endl;
-	cout << "numero de transacciones: " << prueba.getnTx(bloque) << endl;
-	cout << "id de la  transaccion 1: " << prueba.gettxid(tx) << endl;
-	cout << "cant de vin en transaccion 1: " << prueba.getnTxin(tx) << endl;
-	cout << "cant de vout en transaccion 1: " << prueba.getnTxout(tx) << endl;
-	cout << "en vin 1: " << endl;
-	cout << "block id: " << prueba.getblockid(vin) << endl;
-	cout << "tx id: " << prueba.gettxid(vin) << endl;
-	cout << "outputindex: " << prueba.getoutputIndex(vin) << endl;
-	cout << "signature: " << prueba.getsignature(vin) << endl;
-	cout << "en vout 1: " << endl;
-	cout << "amount: " << prueba.getamount(vout) << endl;
-	cout << "publicid: " << prueba.getpublicid(vout) << endl << endl;
-	//prueba.calculatemerkleroot(num);
-
-
 	bool running = true;
 	Gui myGui;
 
